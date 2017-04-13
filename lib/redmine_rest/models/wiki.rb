c0c5b9a61d47dc85445a11ae3e75ea7ec97cd0d5
Txinto Vaz <txinto@gatatac.org>
2017-04-13 23:51:11 +0200

    another try

--- lib/redmine_rest/models/wiki.rb
+++ lib/redmine_rest/models/wiki.rb
@@ -18,12 +18,17 @@ module RedmineRest
       # Overrides parent method.
       # When we want to fetch one wiki, we need not to use prefix
       #
-      #def self.element_path(id, _prefix_options = {}, query_options = nil)
-      #  "/memberships/#{URI.parser.escape id.to_s}#{format_extension}#{query_string(query_options)}"
-      #end
-      #def self.collection_path(title, _prefix_options = {}, query_options = nil)
-      #  self.prefix+"wiki/index#{format_extension}#{query_string(query_options)}"
-      #end      
+      def element_path(id, prefix_options = {}, query_options = nil)
+        check_prefix_options(prefix_options)
+        prefix_options, query_options = split_options(prefix_options) if query_options.nil?
+        "#{prefix(prefix_options)}#{collection_name}/#{URI.parser.escape id.to_s}.#{format.extension}#{query_string(query_options)}"
+      end
+      
+      def collection_path(prefix_options = {}, query_options = nil)
+        check_prefix_options(prefix_options)
+        prefix_options, query_options = split_options(prefix_options) if query_options.nil?
+        "#{prefix(prefix_options)}wiki/index.#{format.extension}#{query_string(query_options)}"
+      end
     end
   end
 end
