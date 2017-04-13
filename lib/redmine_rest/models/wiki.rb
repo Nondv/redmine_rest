a5c348d96ab46050af8ba78e7b3cfa64a5647797
Txinto Vaz <txinto@gatatac.org>
2017-04-14 00:33:40 +0200

    another try

--- lib/redmine_rest/models/wiki.rb
+++ lib/redmine_rest/models/wiki.rb
@@ -2,7 +2,7 @@ require 'active_resource'
 
 require_relative 'collections/base'
 require_relative 'user'
-require_relative 'group'
+require_relative 'project'
 
 module RedmineRest
   module Models
@@ -14,16 +14,23 @@ module RedmineRest
       self.collection_parser = Collections::Base
       self.prefix = '/projects/:project_id/'
 
+      has_one :author, class_name: User
+      has_one :project, class_name: Project
+      
+      validates :title, :tracker_id, presence: true
+      
+      #
+      # Sets #project_id via Project object.
+      #
+      def project=(project)
+        fail ArgumentError unless project.is_a? Project
+        self.project_id = project.id
+      end
+      
       #
       # Overrides parent method.
       # When we want to fetch one wiki, we need not to use prefix
       #
-      def self.element_path(id, prefix_options = {}, query_options = nil)
-        check_prefix_options(prefix_options)
-        prefix_options, query_options = split_options(prefix_options) if query_options.nil?
-        "#{prefix(prefix_options)}#{collection_name}/#{URI.parser.escape id.to_s}.#{format.extension}#{query_string(query_options)}"
-      end
-      
       def self.collection_path(prefix_options = {}, query_options = nil)
         check_prefix_options(prefix_options)
         prefix_options, query_options = split_options(prefix_options) if query_options.nil?
